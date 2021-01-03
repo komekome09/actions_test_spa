d9f3a2712d298e8f4f06cf5f094443f01bad8dc1
komekome09 <fanmaskome@gmail.com>
2021-01-03 23:05:12 +0900

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FlightTable extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={2}>The table header</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>the table body</td>
+            <td>with two column</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
 }
 
-export default App;
+export default class FlightBoard extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <FlightTable />
+        </header>
+      </div>
+    );
+  }
+}
